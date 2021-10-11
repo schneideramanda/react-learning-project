33a85d671001236d2171fc5dfca672646f37b816
Amanda Schneider <schneideramanda01@gmail.com>
2021-10-11 17:15:07 -0300

    Login page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='login/*' element={<Login />} />
+            <ProtectedRoute path='conta/*' element={<User />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
