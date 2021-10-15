a34bd74a3e1462c0f17e46cb1222a1d94601285f
Amanda Schneider <schneideramanda01@gmail.com>
2021-10-15 17:04:30 -0300

    Feed and User Profile added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Feed from './Feed/Feed';
+import Loading from './Helper/Loading';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className='container mainContainer'>
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
